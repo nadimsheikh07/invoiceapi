0672cd90b4800ecd53c9721830874cccb9dade1b
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2021-05-18 10:49:33 +0530

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Item;
+use App\Models\Purchase;
+use App\Models\Sale;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
@@ -17,6 +21,30 @@ class DashboardController extends Controller
             'total' => User::count(),
             'icon' => 'person',
             'url' => 'users'
+        ];
+        $data[] = [
+            'title' => 'Total Categories',
+            'total' => Category::count(),
+            'icon' => 'category',
+            'url' => 'categories'
+        ];
+        $data[] = [
+            'title' => 'Total Items',
+            'total' => Item::count(),
+            'icon' => 'donut_small',
+            'url' => 'items'
+        ];
+        $data[] = [
+            'title' => 'Total Purchase',
+            'total' => Purchase::count(),
+            'icon' => 'inventory',
+            'url' => 'purchases'
+        ];
+        $data[] = [
+            'title' => 'Total Sales',
+            'total' => Sale::count(),
+            'icon' => 'receipt',
+            'url' => 'sales'
         ];
 
 
