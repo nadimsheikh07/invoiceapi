1fb5f1edaf018c745cbf7102b39823bf216621c1
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2021-05-18 11:30:05 +0530

    update dash

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'total_users' => 'Total Users',
+    'total_categories' => 'Total Categories',
+    'total_items' => 'Total Items',
+    'total_purchase' => 'Total Purchase',
+    'total_sales' => 'Total Sales',
+];
